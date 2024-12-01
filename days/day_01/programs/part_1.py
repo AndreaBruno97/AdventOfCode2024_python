d6c64915dfacdda7b023ce4976a331e93f254235
AndreaBruno97 <andbruno97@gmail.com>
2024-12-01 14:28:24 +0100

    Day 1

--- days/day_01/programs/part_1.py
+++ days/day_01/programs/part_1.py
@@ -7,11 +7,23 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
 
-        return -1
+        # Get a list of pairs of numbers.
+        # Inner comprehension casts the strings to integers.
+        pair_list = [[int(y) for y in x.split("   ")] for x in file_lines]
+
+        # Split and sort the left and right lists
+        left_list = [x[0] for x in pair_list]
+        right_list = [x[1] for x in pair_list]
+        left_list.sort()
+        right_list.sort()
+
+        difference_list = [abs(left_value - right_value) for left_value, right_value in zip(left_list, right_list)]
+
+        return sum(difference_list)
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(11)
 p1.execute()
--- days/day_01/programs/part_2.py
+++ days/day_01/programs/part_2.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from common import *
 
 
@@ -7,11 +9,25 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
 
-        return -1
+        # Get a list of pairs of numbers.
+        # Inner comprehension casts the strings to integers.
+        pair_list = [[int(y) for y in x.split("   ")] for x in file_lines]
+
+        # Split the left and right lists
+        left_list = [x[0] for x in pair_list]
+        right_list = [x[1] for x in pair_list]
+
+        # Create a dictionary with the number of occurrences
+        # of each number in the second list
+        occurrences_dictionary = Counter(right_list)
+
+        similarity_list = [x * occurrences_dictionary[x] for x in left_list]
+
+        return sum(similarity_list)
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(31)
 p2.execute()
