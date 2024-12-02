31b08bc4ee8388315fe59daceffbe0652124fc66
AndreaBruno97 <andbruno97@gmail.com>
2024-12-02 19:54:07 +0100

    Day 2

--- days/day_02/programs/part_1.py
+++ days/day_02/programs/part_1.py
@@ -7,11 +7,29 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_list = open_file_lines(filepath)
 
-        return -1
+        safe_report_num = 0
+
+        for index, cur_list in enumerate(input_list):
+            report = [int(x) for x in cur_list.split(" ")]
+            if report[1] - report[0] > 0:
+                # Increase
+                min_difference = 1
+                max_difference = 3
+            else:
+                # Decrease
+                min_difference = -3
+                max_difference = -1
+
+            level_difference = [right - left for (left, right) in zip(report, report[1:])]
+
+            if all(min_difference <= cur_difference <= max_difference for cur_difference in level_difference):
+                safe_report_num += 1
+
+        return safe_report_num
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(2)
 p1.execute()
--- days/day_02/programs/part_2.py
+++ days/day_02/programs/part_2.py
@@ -7,11 +7,32 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_list = open_file_lines(filepath)
 
-        return -1
+        safe_report_num = 0
+
+        for index, cur_list in enumerate(input_list):
+            original_report = [int(x) for x in cur_list.split(" ")]
+
+            for report in [original_report] + [original_report[0:x] + original_report[x+1:] for x in range(len(original_report))]:
+                if report[1] - report[0] > 0:
+                    # Increase
+                    min_difference = 1
+                    max_difference = 3
+                else:
+                    # Decrease
+                    min_difference = -3
+                    max_difference = -1
+
+                level_difference = [right - left for (left, right) in zip(report, report[1:])]
+
+                if all(min_difference <= cur_difference <= max_difference for cur_difference in level_difference):
+                    safe_report_num += 1
+                    break
+
+        return safe_report_num
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(4)
 p2.execute()
