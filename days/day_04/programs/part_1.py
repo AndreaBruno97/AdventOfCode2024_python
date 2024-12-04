771a67c42f39e77fdd79dcdecbcde75a0caa5739
AndreaBruno97 <andbruno97@gmail.com>
2024-12-04 20:00:12 +0100

    Day 4

--- days/day_04/programs/part_1.py
+++ days/day_04/programs/part_1.py
@@ -1,4 +1,7 @@
 from common import *
+import numpy as np
+import re
+
 
 
 class Part_1(BaseClass):
@@ -7,11 +10,49 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        matrix = np.array(open_file_str_matrix(filepath))
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        return -1
+        total_xmas = 0
+
+        # Horizontal
+        for cur_row in ["".join(matrix[i]) for i in range(rows)]:
+            total_xmas += count_xmas(cur_row)
+
+        # Vertical
+        for cur_col in ["".join(matrix[:, i]) for i in range(cols)]:
+            total_xmas += count_xmas(cur_col)
+
+        # Diagonal NE-SW
+        for cur_diag in range(rows + cols):
+            coord_range = range(cur_diag + 1)
+            diag_coordinates = zip(coord_range, coord_range[::-1])
+
+            cur_string = "".join([
+                matrix[r, c] for r, c in diag_coordinates if 0 <= r < rows and 0 <= c < cols
+            ])
+
+            total_xmas += count_xmas(cur_string)
+
+        # Diagonal NW-SE
+        for cur_diag in range(rows + cols):
+            coord_range = range(cur_diag + 1)
+            diag_coordinates = zip(coord_range, coord_range[::-1])
+
+            cur_string = "".join([
+                matrix[rows-1 - r, c] for r, c in diag_coordinates if 0 <= r < rows and 0 <= c < cols
+            ])
+
+            total_xmas += count_xmas(cur_string)
+
+        return total_xmas
+
+
+def count_xmas(cur_string):
+    return len(re.findall("XMAS", cur_string) + re.findall("SAMX", cur_string))
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(18)
 p1.execute()
--- days/day_04/programs/part_2.py
+++ days/day_04/programs/part_2.py
@@ -1,4 +1,5 @@
 from common import *
+import numpy as np
 
 
 class Part_2(BaseClass):
@@ -7,11 +8,38 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        xmas = np.array([
+            ["M", "", "S"],
+            ["", "A", ""],
+            ["M", "", "S"]
+        ])
 
-        return -1
+        matrix = np.array(open_file_str_matrix(filepath))
+        rows = len(matrix)
+        cols = len(matrix[0])
+        xmas_rows = len(xmas)
+        xmas_cols = len(xmas[0])
+
+        # All possible orientations of X-MAS matrix
+        # Then each matrix is flattened, to help future checks
+        xmas_list = [
+            xmas.flatten(),
+            xmas.transpose().flatten(),
+            xmas[:, ::-1].flatten(),
+            xmas[:, ::-1].transpose().flatten()
+        ]
+        total_xmas = 0
+
+        for r in range(rows - xmas_rows + 1):
+            for c in range(cols - xmas_cols + 1):
+                sub_matrix = matrix[r:r+xmas_rows, c:c+xmas_cols]
+                sub_matrix_flat = sub_matrix.flatten()
+                for cur_xmas in xmas_list:
+                    if np.all([ref == "" or ref == sub for ref, sub in zip(cur_xmas, sub_matrix_flat)]):
+                        total_xmas += 1
+        return total_xmas
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(9)
 p2.execute()
