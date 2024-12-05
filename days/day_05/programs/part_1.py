761e614ac4bc71da11dea8b9bc93a47fac1a3824
AndreaBruno97 <andbruno97@gmail.com>
2024-12-05 20:46:19 +0100

    Day 5

--- days/day_05/programs/part_1.py
+++ days/day_05/programs/part_1.py
@@ -1,4 +1,5 @@
 from common import *
+import math
 
 
 class Part_1(BaseClass):
@@ -7,11 +8,43 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_str = open_file(filepath)
+        rule_str, update_str = file_str.split("\n\n")
 
-        return -1
+        rule_list = [rule.split("|") for rule in rule_str.split("\n")]
+        update_list = [update.split(",") for update in update_str.split("\n")]
+
+        total = 0
+
+        for update in update_list:
+            # Searching the i-th element for a broken rule
+            # Not useful to test the first element
+            is_correct = True
+
+            for i in range(1, len(update)):
+                for k in range(i):
+                    # i is the current element in the update list
+                    # k is one of the previous elements we are testing
+                    #
+                    # The correct rule would have the element in i AFTER the element in k
+                    # But if we find the rule with i BEFORE k, the update list is incorrect
+
+                    if [update[i], update[k]] in rule_list:
+                        is_correct = False
+                        break
+
+                if not is_correct:
+                    break
+
+            if is_correct:
+                # The current update is correct, extract the middle element
+                middle_index = math.floor(len(update)/2)
+                middle_element = int(update[middle_index])
+                total += middle_element
+
+        return total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(143)
 p1.execute()
--- days/day_05/programs/part_2.py
+++ days/day_05/programs/part_2.py
@@ -1,4 +1,6 @@
 from common import *
+import math
+import numpy as np
 
 
 class Part_2(BaseClass):
@@ -7,11 +9,78 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_str = open_file(filepath)
+        rule_str, update_str = file_str.split("\n\n")
 
-        return -1
+        rule_list = [rule.split("|") for rule in rule_str.split("\n")]
+        update_list = [update.split(",") for update in update_str.split("\n")]
 
+        total = 0
+
+        for update in update_list:
+            if not is_sorted(update, rule_list):
+                # Re-ordering the update
+                new_update = []
+                option_list = [*update]
+                while len(new_update) < len(update):
+                    for option in option_list:
+                        check_list = [[option, x] for x in option_list if option is not x]
+
+                        if np.all([x in rule_list for x in check_list]):
+                            new_update.append(option)
+                            option_list = [x for x in option_list if option is not x]
+                            break
+
+                update = new_update
+
+                # The current update is now correct, extract the middle element
+                middle_index = math.floor(len(update) / 2)
+                middle_element = int(update[middle_index])
+                total += middle_element
+
+        return total
+
+
+def is_sorted(update, rule_list):
+    # Searching the i-th element for a broken rule
+    # Not useful to test the first element
+    for i in range(1, len(update)):
+        for k in range(i):
+            # i is the current element in the update list
+            # k is one of the previous elements we are testing
+            #
+            # The correct rule would have the element in i AFTER the element in k
+            # But if we find the rule with i BEFORE k, the update list is incorrect
+
+            if [update[i], update[k]] in rule_list:
+                return False
+
+    return True
+
+
+'''
+def get_sorted_update(cur_update, option_list, rule_list):
+    if len(option_list) == 0:
+        return cur_update
+
+    for option in option_list:
+        is_valid = True
+        for old_update in cur_update:
+            if [option, old_update] in rule_list:
+                is_valid = False
+                break
+
+        if is_valid:
+            new_update = [*cur_update, option]
+            new_option_list = [x for x in option_list if x is not option]
+            result = get_sorted_update(new_update, new_option_list, rule_list)
+
+            if len(result) > 0:
+                return result
+
+    return []
+'''
 
 p2 = Part_2()
-p2.test(0)
+p2.test(123)
 p2.execute()
