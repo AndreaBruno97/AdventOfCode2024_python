6c19c2f73414752c285a9554c3161b209e8753ce
AndreaBruno97 <andbruno97@gmail.com>
2024-12-07 10:14:24 +0100

    Day 7

--- days/day_07/programs/part_1.py
+++ days/day_07/programs/part_1.py
@@ -7,11 +7,40 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        str_list = [x.split(": ") for x in open_file_lines(filepath)]
+        eq_list = [
+            [
+                int(tot),
+                [
+                    int(param)
+                    for param in param_str.split(" ")
+                ]
+            ] for tot, param_str in str_list
+        ]
 
-        return -1
+        total_sum = 0
+        for total, param_list in eq_list:
+            cur_total_results = check_operators(total, param_list[0], param_list[1:])
+            if cur_total_results > 0:
+                total_sum += total
+
+        return total_sum
+
+
+def check_operators(total, cur_total, param_list):
+    if len(param_list) == 0:
+        if total == cur_total:
+            return 1
+        else:
+            return 0
+
+    cur_total_results = 0
+    cur_total_results += check_operators(total, cur_total + param_list[0], param_list[1:])
+    cur_total_results += check_operators(total, cur_total * param_list[0], param_list[1:])
+
+    return cur_total_results
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(3749)
 p1.execute()
--- days/day_07/programs/part_2.py
+++ days/day_07/programs/part_2.py
@@ -7,11 +7,41 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        str_list = [x.split(": ") for x in open_file_lines(filepath)]
+        eq_list = [
+            [
+                int(tot),
+                [
+                    int(param)
+                    for param in param_str.split(" ")
+                ]
+            ] for tot, param_str in str_list
+        ]
 
-        return -1
+        total_sum = 0
+        for total, param_list in eq_list:
+            cur_total_results = check_operators(total, param_list[0], param_list[1:])
+            if cur_total_results > 0:
+                total_sum += total
+
+        return total_sum
+
+
+def check_operators(total, cur_total, param_list):
+    if len(param_list) == 0:
+        if total == cur_total:
+            return 1
+        else:
+            return 0
+
+    cur_total_results = 0
+    cur_total_results += check_operators(total, cur_total + param_list[0], param_list[1:])
+    cur_total_results += check_operators(total, cur_total * param_list[0], param_list[1:])
+    cur_total_results += check_operators(total, int(str(cur_total) + str(param_list[0])), param_list[1:])
+
+    return cur_total_results
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(11387)
 p2.execute()
