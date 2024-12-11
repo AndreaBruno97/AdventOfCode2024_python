59b0bd4695e91111602838992b153aef0aa0b51b
AndreaBruno97 <andbruno97@gmail.com>
2024-12-11 19:13:41 +0100

    Day 11

--- days/day_11/programs/part_1.py
+++ days/day_11/programs/part_1.py
@@ -1,17 +1,40 @@
 from common import *
 
 
+ROUNDS = 25
+
+
 class Part_1(BaseClass):
 
     def __init__(self):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        stone_line = [int(x) for x in open_file(filepath).split(" ")]
 
-        return -1
+        for cur_round in range(ROUNDS):
+            i = 0
+            while i in range(len(stone_line)):
+                stone_val = stone_line[i]
+                num_digits = len(str(stone_val))
+
+                if stone_val == 0:
+                    stone_line[i] = 1
+                elif num_digits % 2 == 0:
+                    # Even number of digits
+                    middle_power_ten = pow(10, int(num_digits / 2))
+                    stone_line[i] = int(stone_val / middle_power_ten)
+                    stone_line.insert(0, int(stone_val % middle_power_ten))
+                    i += 1
+                else:
+                    # Odd number of digits
+                    stone_line[i] = stone_val * 2024
+                i += 1
+            print("Round ", cur_round + 1)
+
+        return len(stone_line)
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(55312)
 p1.execute()
--- days/day_11/programs/part_2.py
+++ days/day_11/programs/part_2.py
@@ -1,17 +1,49 @@
 from common import *
 
 
+ROUNDS = 75
+stone_dict = dict()
+
+
 class Part_2(BaseClass):
 
     def __init__(self):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        stone_line = [int(x) for x in open_file(filepath).split(" ")]
 
-        return -1
+        total = 0
+        for stone in stone_line:
+            total += search_stone(stone, 0)
 
+        return total
+
+
+def search_stone(stone_val, depth):
+    if depth == ROUNDS:
+        return 1
+
+    if (stone_val, depth) in stone_dict.keys():
+        return stone_dict[(stone_val, depth)]
+
+    num_digits = len(str(stone_val))
+    total = 0
+    if stone_val == 0:
+        total = search_stone(1, depth + 1)
+    elif num_digits % 2 == 0:
+        # Even number of digits
+        middle_power_ten = pow(10, int(num_digits / 2))
+
+        total += search_stone(int(stone_val / middle_power_ten), depth + 1)
+        total += search_stone( int(stone_val % middle_power_ten), depth + 1)
+    else:
+        # Odd number of digits
+        total = search_stone(stone_val * 2024, depth + 1)
+
+    stone_dict[(stone_val, depth)] = total
+    return total
 
 p2 = Part_2()
-p2.test(0)
+#p2.test(0)
 p2.execute()
