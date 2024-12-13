0d11c33c35abb7835d51be602300e8bc436827cd
AndreaBruno97 <andbruno97@gmail.com>
2024-12-13 20:55:10 +0100

    Day 13

--- days/day_13/programs/part_1.py
+++ days/day_13/programs/part_1.py
@@ -1,4 +1,9 @@
 from common import *
+import re
+import sys
+
+
+MAX_PRESS = 100
 
 
 class Part_1(BaseClass):
@@ -7,11 +12,41 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_str = open_file(filepath)
+        pattern = r"Button A: X\+(\d+), Y\+(\d+)\n" \
+                  r"Button B: X\+(\d+), Y\+(\d+)\n" \
+                  r"Prize: X=(\d+), Y=(\d+)"
 
-        return -1
+        machine_list = [[int(y) for y in x]for x in re.findall(pattern, input_str)]
+
+        total = 0
+        for a_delta_x, a_delta_y, b_delta_x, b_delta_y, prize_x, prize_y in machine_list:
+            cur_total = sys.maxsize
+            for a in range(1, MAX_PRESS+1):
+                x_a, y_a = a*a_delta_x, a*a_delta_y
+                if x_a > prize_x or y_a > prize_y:
+                    break
+
+                b = (prize_x - x_a) / b_delta_x
+
+                if not b.is_integer() or b > MAX_PRESS:
+                    continue
+                b = int(b)
+
+                if y_a + (b * b_delta_y) != prize_y:
+                    continue
+
+                num_token = (3 * a) + b
+
+                if num_token < cur_total:
+                    cur_total = num_token
+
+            if cur_total < sys.maxsize:
+                total += cur_total
+
+        return total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(480)
 p1.execute()
--- days/day_13/programs/part_2.py
+++ days/day_13/programs/part_2.py
@@ -1,4 +1,6 @@
 from common import *
+import re
+import sys
 
 
 class Part_2(BaseClass):
@@ -7,9 +9,40 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_str = open_file(filepath)
+        pattern = r"Button A: X\+(\d+), Y\+(\d+)\n" \
+                  r"Button B: X\+(\d+), Y\+(\d+)\n" \
+                  r"Prize: X=(\d+), Y=(\d+)"
 
-        return -1
+        machine_list = [[int(y) for y in x]for x in re.findall(pattern, input_str)]
+
+        total = 0
+        for a_x, a_y, b_x, b_y, prize_x, prize_y in machine_list:
+            prize_x += 10000000000000
+            prize_y += 10000000000000
+
+            delta = (a_x * b_y) - (a_y * b_x)
+
+            if delta == 0:
+                continue
+
+            b = ((a_x * prize_y) - (a_y * prize_x)) / delta
+
+            if not b.is_integer():
+                continue
+
+            b = int(b)
+
+            a = (prize_x - (b_x * b)) / a_x
+
+            if not a.is_integer():
+                continue
+
+            a = int(a)
+
+            total += (3 * a) + b
+
+        return total
 
 
 p2 = Part_2()
