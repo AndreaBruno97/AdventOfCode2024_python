d4e6d196dd76c51e93a0740487d39c221500aca7
AndreaBruno97 <andbruno97@gmail.com>
2024-12-14 15:19:43 +0100

    Day 14

--- days/day_14/programs/part_1.py
+++ days/day_14/programs/part_1.py
@@ -1,4 +1,8 @@
 from common import *
+import math
+import re
+from functools import reduce
+from operator import mul
 
 
 class Part_1(BaseClass):
@@ -7,11 +11,41 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
 
-        return -1
+        MAX_X = 101
+        MAX_Y = 103
+        STEP_NUM = 100
+
+        if "input.txt" not in filepath:
+            MAX_X = 11
+            MAX_Y = 7
+
+        file_str = open_file(filepath)
+        pattern = r"p=(\-*\d+),(\-*\d+) v=(\-*\d+),(\-*\d+)"
+        guard_list = [[int(y) for y in x] for x in re.findall(pattern, file_str)]
+
+        quadrant_dict = dict({
+            ((False, False), 0),
+            ((False, True), 0),
+            ((True, False), 0),
+            ((True, True), 0)
+        })
+
+        middle_x = math.floor(MAX_X/2)
+        middle_y = math.floor(MAX_Y/2)
+
+        for p_x, p_y, v_x, v_y in guard_list:
+            end_x = (p_x + (v_x * STEP_NUM)) % MAX_X
+            end_y = (p_y + (v_y * STEP_NUM)) % MAX_Y
+
+            if end_x == middle_x or end_y == middle_y:
+                continue
+
+            quadrant_dict[(end_x < middle_x, end_y < middle_y)] += 1
+
+        return reduce(mul, quadrant_dict.values())
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(12)
 p1.execute()
--- days/day_14/programs/part_2.py
+++ days/day_14/programs/part_2.py
@@ -1,4 +1,6 @@
 from common import *
+import re
+import time
 
 
 class Part_2(BaseClass):
@@ -7,11 +9,46 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+
+        MAX_X = 101
+        MAX_Y = 103
+
+        if "input.txt" not in filepath:
+            MAX_X = 11
+            MAX_Y = 7
+
+        file_str = open_file(filepath)
+        pattern = r"p=(\-*\d+),(\-*\d+) v=(\-*\d+),(\-*\d+)"
+        guard_list = [[int(y) for y in x] for x in re.findall(pattern, file_str)]
+
+        steps = 0
+        while True:
+            guard_set = set()
+            for p_x, p_y, v_x, v_y in guard_list:
+                end_x = (p_x + (v_x * steps)) % MAX_X
+                end_y = (p_y + (v_y * steps)) % MAX_Y
+
+                guard_set.add((end_x, end_y))
+
+            print_map(MAX_X, MAX_Y, guard_set)
+            print(steps)
+            time.sleep(0.1)
+
+            steps += 1
 
         return -1
 
 
+def print_map(max_x, max_y, guard_set):
+    print_error("-"*max_y)
+    for y in range(max_y):
+        for x in range(max_x):
+            if (x, y) in guard_set:
+                print_success("*", end="")
+            else:
+                print(" ", end="")
+        print("")
+
+
 p2 = Part_2()
-p2.test(0)
 p2.execute()
