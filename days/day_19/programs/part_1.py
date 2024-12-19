d30fd5e6c6ec56997bce780eb0fb8729b519cd40
AndreaBruno97 <andbruno97@gmail.com>
2024-12-19 21:13:09 +0100

    Day 19

--- days/day_19/programs/part_1.py
+++ days/day_19/programs/part_1.py
@@ -7,11 +7,39 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        towel_list_str, pattern_list_str = open_file(filepath).split("\n\n")
+        towel_set = set(towel_list_str.split(", "))
+        pattern_list = pattern_list_str.split("\n")
+        impossible_pattern_set = set()
 
-        return -1
+        total = 0
+        for i, pattern in enumerate(pattern_list):
+            is_possible = create_pattern(pattern, towel_set, impossible_pattern_set)
+            if is_possible:
+                total += 1
+
+        return total
+
+
+def create_pattern(pattern, towel_set, impossible_pattern_set):
+    if len(pattern) == 0 or pattern in towel_set:
+        return True
+
+    if pattern in impossible_pattern_set:
+        return False
+
+    for towel in towel_set:
+        tow_len = len(towel)
+        if pattern[:tow_len] == towel:
+            is_possible = create_pattern(pattern[tow_len:], towel_set, impossible_pattern_set)
+            if is_possible:
+                towel_set.add(pattern)
+                return True
+
+    impossible_pattern_set.add(pattern)
+    return False
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(6)
 p1.execute()
--- days/day_19/programs/part_2.py
+++ days/day_19/programs/part_2.py
@@ -7,11 +7,37 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        towel_list_str, pattern_list_str = open_file(filepath).split("\n\n")
+        towel_list = towel_list_str.split(", ")
+        pattern_list = pattern_list_str.split("\n")
 
-        return -1
+
+        towel_dict = dict()
+
+        total = 0
+        for i, pattern in enumerate(pattern_list):
+            total += count_pattern(pattern, towel_list, towel_dict)
+
+        return total
+
+
+def count_pattern(pattern, towel_list, towel_dict):
+    if pattern in towel_dict:
+        return towel_dict[pattern]
+
+    if len(pattern) == 0:
+        return 1
+
+    total_count = 0
+    for towel in towel_list:
+        tow_len = len(towel)
+        if pattern[:tow_len] == towel:
+            total_count += count_pattern(pattern[tow_len:], towel_list, towel_dict)
+
+    towel_dict[pattern] = total_count
+    return total_count
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(16)
 p2.execute()
