3ea8c98f6b137668cdac3d95542e59f7a97cfbfe
AndreaBruno97 <andbruno97@gmail.com>
2024-12-23 12:39:43 +0100

    Day 23 - Part 1

--- days/day_23/programs/part_1.py
+++ days/day_23/programs/part_1.py
@@ -1,3 +1,5 @@
+import itertools
+
 from common import *
 
 
@@ -7,11 +9,34 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        edge_list = [x.split("-") for x in open_file_lines(filepath)]
 
-        return -1
+        lan_map = dict()
+        node_set = set()
+
+        for left, right in edge_list:
+            if left not in lan_map:
+                lan_map[left] = []
+            if right not in lan_map:
+                lan_map[right] = []
+
+            lan_map[left].append(right)
+            lan_map[right].append(left)
+
+            node_set.add(left)
+            node_set.add(right)
+
+        sub_lan_set = set()
+
+        for cur_node, cur_neighbor_list in lan_map.items():
+            for cur_neigh in cur_neighbor_list:
+                for neigh_neighbor in lan_map[cur_neigh]:
+                    if neigh_neighbor in cur_neighbor_list and "t" in cur_node[0] + cur_neigh[0] + neigh_neighbor[0]:
+                        sub_lan_set.add(frozenset([cur_node, cur_neigh, neigh_neighbor]))
+
+        return len(sub_lan_set)
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(7)
 p1.execute()
